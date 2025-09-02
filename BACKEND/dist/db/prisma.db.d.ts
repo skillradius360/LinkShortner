0ce1fe4912d7b3870b7bb3650ebf03f848e1d6a4
skillradius360 <syncmasterx@gmail.com>
2025-09-03 01:53:13 +0530

    config

--- BACKEND/dist/db/prisma.db.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-export declare const client: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-//# sourceMappingURL=prisma.db.d.ts.map
--- BACKEND/dist/routes/shortner.routes.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const shortnerRouter: import("express-serve-static-core").Router;
-//# sourceMappingURL=shortner.routes.d.ts.map
